f57c2819f578f110b75f559852d5d046a42ff0b9
Steven Cooper <stevenleoncooper@gmail.com>
2021-05-18 14:28:08 -0700

    More advanced selector options for binding.

--- scripts/helper_bindings.js
+++ scripts/helper_bindings.js
@@ -5,11 +5,11 @@ export const appBindings = {};
 export class binding {
     constructor(sourceElement, bindingString) {
 
-        let values = bindingString.split(" of ").join(",").split(" with ").join(",").split(" ").join(",").split(",");
+        let values = bindingString.split(" of ").join(",").split(" with ").join(",").split(": ").join(",").split(",");
 
         this.method = values[0];
         this.targetData = values[1];
-        this.targetSelector = values[2];
+        this.targetSelector = values[2].replace("{","").replace("}","");
         this.sourceData = values[3];
         this.source = sourceElement;
         this.history = [];
