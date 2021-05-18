043c491f7b7fd8deb2c2ad63f12b712f5fbb1c54
Steven Cooper <stevenleoncooper@gmail.com>
2021-05-18 11:12:41 -0700

    Experimenting with 1-way binding.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,103 @@
-import ajax from "./helper_ajax.js";
+const UI = { set: {}, get: {} };
 
-const app = {
-    ajax: ajax
-};
+UI.get = {
+    innerHTML: (source) => {
+        return source.innerHTML;
+    },
+    textContent: (source) => {
+        return source.textContent;
+    },
+    value: (source) => {
+        return source.value;
+    },
+}
 
-window.test = app.ajax.test;
+UI.set = {
+    innerHTML: (target, data) => {
+        target.innerHTML = data;
+        return `target HTML is now ${data}`;
+    },
+    textContent: (target, data) => {
+        target.textContent = data;
+        return `target text is now ${data}`;
+    },
+    value: (target, data) => {
+        target.value = data;
+        return `target value is now ${data}`;
+    }
+}
+
+class binding {
+    constructor(method, to, target, from, source) {
+        this.method = method;
+        this.targetSelector = target;
+        this.targetData = to;
+        this.sourceData = from;
+        this.source = source;
+        this.history = [];
+
+    }
+
+    update = () => {
+
+        let target = document.querySelector(this.targetSelector);
+
+        let data = UI.get[this.sourceData]?.(this.source);
+
+        let result = UI.set[this.targetData]?.(target, data);
+
+        this.history.push(result);
+
+        console.log("test");
+    };
+}
+
+
+const processBinding = (source, bindingString) => {
+
+    if (!bindingString) return false;
+
+    let values = bindingString.split(" of ").join(",").split(" with ").join(",").split(" ").join(",").split(",");
+
+    let output = new binding(values[0], values[1], values[2], values[3], source);
+
+    return output;
+}
+
+
+
+document.body.addEventListener("keyup", (e) => {
+
+    let source = e.target;
+
+    if (!source.dataset.keyup) return false;
+
+    let binding = processBinding(source, source.dataset.keyup);
+
+    binding.update();
+
+});
+
+document.body.addEventListener("change", (e) => {
+
+    let source = e.target;
+
+    if (!source.dataset.change) return false;
+
+    let binding = processBinding(source, source.dataset.change);
+
+    binding.update();
+
+});
+
+document.body.addEventListener("click", (e) => {
+
+    let source = e.target;
+
+    if (!source.dataset.click) return false;
+
+    let binding = processBinding(source, source.dataset.click);
+
+    Actions.click[binding.subAction]?.(source, e);
+
+});
