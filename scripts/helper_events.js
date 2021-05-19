3a8faba92bfe938beb68a2ac2963de103d4a3858
Steven Cooper <stevenleoncooper@gmail.com>
2021-05-18 21:38:16 -0700

    Add/Remove column and row options. Not fully correct yet for deleting columns.

--- scripts/helper_events.js
+++ scripts/helper_events.js
@@ -2,3 +2,29 @@ export const events = {
     click: {},
     change: {}
 };
+
+events.click.addColumn = (source, e) => {
+
+    let template = document.querySelector("#ExtraColumnTemplate");
+
+    let newContent = template.content.cloneNode(true);
+
+    let thisRow = source.parentNode.parentNode;
+    
+    thisRow.appendChild(newContent); 
+
+    let headRow = document.querySelector("#out_thead tr");
+
+    let currentCount = headRow.children.length;
+    let newCount = thisRow.children.length;
+
+    if(newCount > currentCount){
+        headRow.innerHTML = headRow.innerHTML + "<th>Extra Column</th>";
+    }
+};
+
+events.click.deleteColumn = (source, e) =>{
+
+    source.parentNode.parentNode.removeChild(source.parentNode);
+
+};
