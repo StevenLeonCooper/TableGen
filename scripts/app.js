28f9b6895dc8d19e5c031d30c2188caad65bae71
Steven Cooper <stevenleoncooper@gmail.com>
2021-05-20 15:25:56 -0700

    Added "SimpleTable" class. CHANGES EVERYTHING! This will render most of my old code useless!

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,80 @@ import { events } from './helper_events.js';
 import { UI } from './helper_ui.js';
 import mustache from './libs/mustache.js';
 
+
+class SimpleTable {
+    constructor(name) {
+        this.name = name;
+        this.caption = "Table Caption";
+        this.tableHeading = [
+            "heading"
+        ];
+        this.tableBody = [
+            ["value"]
+        ];
+    }
+
+    getColumnCount() {
+        return this.tableHeading.length;
+    }
+
+    getRowCount() {
+        return this.tableBody.length;
+    }
+
+    addColumn() {
+        this.tableHeading.push("New Heading");
+        this.tableBody.forEach((item, index) => {
+            item.push("New Value");
+        });
+    }
+    addRow() {
+        let newRow = [];
+
+        this.tableHeading.forEach((item, idex) => {
+            newRow.push("New Value");
+        });
+        this.tableBody.push(newRow);
+    }
+
+    removeColumn(index) {
+        this.tableHeading.splice(index, 1);
+        this.tableBody.forEach((item, i) => {
+            item.splice(index, 1);
+        });
+    }
+
+    removeRow(index) {
+        this.tableBody.splice(index, 1);
+    }
+
+    getHtml() {
+        let headerHtml = this.tableHeading.map(value => `<th>${value}</th>`).join(' ');
+        let bodyHtml = "";
+        this.tableBody.forEach((item) => {
+            bodyHtml += ("<tr>" + item.map(value => `<td>${value}</td>`).join(' ') + "</tr>");
+        });
+
+        let outHtml = `<table>
+                        <caption>${this.caption}</caption>
+                        <thead><tr>${headerHtml}</tr></thead>
+                        <tbody>${bodyHtml}</tbody>
+                        </table>`;
+
+        return outHtml;
+    }
+
+    updateTableBody(row, column, value) {
+        if (this.tableBody?.[row]?.[column]) {
+            this.tableBody[row][column] = value;
+        }
+    }
+
+}
+
+export const mainTable = new SimpleTable("Main");
+window._mainTable = mainTable;
+
 class context {
 
     constructor() {
